631c96630c4ab99e36cd2af177a7c716e2733f74
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-01 13:01:31 -0500

    update

---
+++ day1/main.rb
@@ -0,0 +1,8 @@
+# require "active_support/all"
+
+first, second = File.read("input.txt").lines.map(&:split).transpose
+first, second = [first, second].map { |e| e.map(&:to_i) }.map(&:sort)
+
+p(first.zip(second).sum { |a, b| (a - b).abs })
+
+p(first.sum { |n| n * second.count(n) })

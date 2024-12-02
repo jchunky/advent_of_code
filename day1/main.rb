6f77ed45b8cbd1ab169a951ef6b8eba0090a33b3
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-02 01:40:00 -0500

    update

--- day1/main.rb
+++ day1/main.rb
@@ -1,4 +1,4 @@
-# require "active_support/all"
+require "active_support/all"
 
 first, second = File.read("input.txt").lines.map(&:split).transpose
 first, second = [first, second].map { |e| e.map(&:to_i) }.map(&:sort)
---
+++ day2/main.rb
@@ -0,0 +1,10 @@
+require "active_support/all"
+
+def safe?(report)
+  return false unless [report.sort, report.sort.reverse].include?(report)
+
+  report.each_cons(2).all? { |a, b| (a - b).abs.in?(1..3) }
+end
+
+reports = File.read("input.txt").lines.map(&:split).map { |e| e.map(&:to_i) }
+p reports.count { safe?(_1) }

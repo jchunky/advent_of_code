c7837550448e1ea836fb07fbc34b0ba98956c4fb
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-04 12:24:36 -0500

    refactor

--- day4/main.rb
+++ day4/main.rb
@@ -11,20 +11,20 @@ row_count = rows.count
 col_count = rows.first.count
 
 directions = [
-  [[0, 0], [0, 1], [0, 2], [0, 3]],
-  [[0, 3], [0, 2], [0, 1], [0, 0]],
-  [[0, 0], [1, 0], [2, 0], [3, 0]],
-  [[3, 0], [2, 0], [1, 0], [0, 0]],
-  [[0, 0], [1, 1], [2, 2], [3, 3]],
-  [[3, 3], [2, 2], [1, 1], [0, 0]],
-  [[3, 0], [2, 1], [1, 2], [0, 3]],
-  [[0, 3], [1, 2], [2, 1], [3, 0]],
+  [[0, 0], [0, 1], [0, 2], [0, 3]], # horizontal line
+  [[0, 0], [1, 0], [2, 0], [3, 0]], # vertical line
+  [[0, 0], [1, 1], [2, 2], [3, 3]], # falling diagonal
+  [[3, 0], [2, 1], [1, 2], [0, 3]], # rising diagonal
 ]
+word = "XMAS"
+words = [word, word.reverse]
 count =
-  directions.sum do |coords|
-    row_count.times.sum do |r|
-      col_count.times.count do |c|
-        find_word?(rows, r, c, coords, "XMAS")
+  words.sum do |word|
+    directions.sum do |coords|
+      row_count.times.sum do |r|
+        col_count.times.count do |c|
+          find_word?(rows, r, c, coords, word)
+        end
       end
     end
   end
@@ -36,10 +36,10 @@ count =
   words.product(words).sum do |word1, word2|
     row_count.times.sum do |r|
       col_count.times.count do |c|
-        coords1 = [[0, 0], [1, 1], [2, 2]]
+        coords1 = [[0, 0], [1, 1], [2, 2]] # falling diagonal
         found1 = find_word?(rows, r, c, coords1, word1)
 
-        coords2 = [[2, 0], [1, 1], [0, 2]]
+        coords2 = [[2, 0], [1, 1], [0, 2]] # firsing diagonal
         found2 = find_word?(rows, r, c, coords2, word2)
 
         found1 && found2

3cae7fa225cd49a92adaf9312bfbd8fdde3e76f1
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-05 16:52:51 -0500

    refactor

--- 2015/day2/main.rb
+++ 2015/day2/main.rb
@@ -5,5 +5,13 @@ def required_wrapping_paper(line)
   (a * b) + (2 * ((a * b) + (b * c) + (c * a)))
 end
 
+def required_ribbon(line)
+  a, b, c = line.split("x").map(&:to_i).sort
+  (2 * (a + b)) + (a * b * c)
+end
+
 lines = File.readlines("input.txt")
+
 p(lines.sum { |l| required_wrapping_paper(l) })
+
+p(lines.sum { |l| required_ribbon(l) })

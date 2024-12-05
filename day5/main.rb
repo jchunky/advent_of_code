8bedde4cd08fd8174a159f738b90551194cb28f9
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-05 05:00:29 -0500

    refactor

--- day5/main.rb
+++ day5/main.rb
@@ -18,13 +18,31 @@ def middle(update)
   update[update.length / 2]
 end
 
+def find_correct_permutation(rules, update)
+  count = update.length
+  update = update.dup
+  result = []
+  rules = rules.select { |a, b| update.include?(a) && update.include?(b) }
+  while result.length < count
+    next_number = update.find { |n| rules.map(&:last).exclude?(n) }
+    result << next_number
+    update.delete(next_number)
+    rules = rules.reject { |a, b| a == next_number }
+  end
+  result
+end
+
 rules, updates = File.read("input.txt").split("\n\n")
 rules = rules.split("\n").map { |r| r.split("|").map(&:to_i) }
 updates = updates.split("\n").map { |u| u.split(",").map(&:to_i) }
 
-result =
-  updates
-    .select { |u| correct?(rules, u) }
-    .sum { |u| middle(u) }
+result = updates
+  .select { |u| correct?(rules, u) }
+  .sum { |u| middle(u) }
+p result # 5331
 
-p result # 5588
+result = updates
+  .reject { |u| correct?(rules, u) }
+  .map { |u| find_correct_permutation(rules, u) }
+  .sum { |u| middle(u) }
+p result

a762967d6fd9e54e80ccab090bf4939293d9801c
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-05 18:17:41 -0500

    refactor

---
+++ 2015/day5/main.rb
@@ -0,0 +1,15 @@
+require "active_support/all"
+
+DUPLICATES = ("a".."z").map { |l| l * 2 }
+BANNED = %w[ab cd pq xy]
+
+def nice?(string)
+  enough_vowels = string.chars.count { |c| %w[a e i o u].include?(c) } >= 3
+  duplicate = DUPLICATES.any? { |d| string.include?(d) }
+  banned = BANNED.any? { |b| string.include?(b) }
+  enough_vowels && duplicate && !banned
+end
+
+strings = File.readlines("input.txt").map(&:chomp)
+
+p(strings.count { |s| nice?(s) }) # 258

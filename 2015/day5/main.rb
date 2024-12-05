d4245200f78c2f33917a7293dd321502ad258d77
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-05 18:24:47 -0500

    refactor

--- 2015/day5/main.rb
+++ 2015/day5/main.rb
@@ -10,6 +10,20 @@ def nice?(string)
   enough_vowels && duplicate && !banned
 end
 
+def improved_nice?(string)
+  duplicate_pair?(string) && pair_sandwich?(string)
+end
+
+def duplicate_pair?(string)
+  0.upto(string.length - 3).any? { |i| string[i + 2..].include?(string[i, 2]) }
+end
+
+def pair_sandwich?(string)
+  0.upto(string.length - 3).any? { |i| string[i] == string[i + 2] }
+end
+
 strings = File.readlines("input.txt").map(&:chomp)
 
 p(strings.count { |s| nice?(s) }) # 258
+
+p(strings.count { |s| improved_nice?(s) }) # 53

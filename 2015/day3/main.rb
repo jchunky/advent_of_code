24d50416d9e419e96ec9b3f2561fde3a7f4f6322
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-05 18:01:34 -0500

    refactor

--- 2015/day3/main.rb
+++ 2015/day3/main.rb
@@ -4,8 +4,7 @@ input = File.read("input.txt").chars
 
 position = [0, 0]
 visits = Hash.new(0)
-visits[position] += 1
-
+visits[position.to_s] += 1
 input.each do |c|
   case c
   when "^"
@@ -17,8 +16,23 @@ input.each do |c|
   when "<"
     position[1] -= 1
   end
-
-  visits[position] += 1
+  visits[position.to_s] += 1
 end
+p visits.keys.count # 2081
 
-p visits.keys.count
+santa_position = [0, 0]
+robo_position = [0, 0]
+visits = Hash.new(0)
+visits[santa_position.to_s] += 1
+visits[robo_position.to_s] += 1
+input.each.with_index(1) do |c, i|
+  position = i.odd? ? santa_position : robo_position
+  case c
+  when "^" then position[0] += 1
+  when "v" then position[0] -= 1
+  when ">" then position[1] += 1
+  when "<" then position[1] -= 1
+  end
+  visits[position.to_s] += 1
+end
+p visits.keys.count # 2341

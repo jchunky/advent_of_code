4c82115334752ce65fee190ac4dc2a465cf78b7a
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-05 22:43:41 -0500

    refactor

--- 2016/day1/main.rb
+++ 2016/day1/main.rb
@@ -1,17 +1,67 @@
 require "active_support/all"
 
-def required_wrapping_paper(line)
-  a, b, c = line.split("x").map(&:to_i).sort
-  (a * b) + (2 * ((a * b) + (b * c) + (c * a)))
+instructions = File.read("input.txt").chomp
+
+bearing = %i[north east south west]
+position = [0, 0]
+instructions.split(", ").each do |instruction|
+  turn, *steps = instruction.chars
+  steps = steps.join.to_i
+  case turn
+  when "L" then bearing.rotate!(-1)
+  when "R" then bearing.rotate!(+1)
+  end
+  case bearing.first
+  when :north then position[0] += steps
+  when :east then position[1] += steps
+  when :south then position[0] -= steps
+  when :west then position[1] -= steps
+  end
+end
+p position.sum(&:abs)
+
+def take_steps(visits, position, bearing, steps)
+  case bearing.first
+  when :north
+    steps.times do
+      position[0] += 1
+      record_position(visits, position)
+    end
+  when :east
+    steps.times do
+      position[1] += 1
+      record_position(visits, position)
+    end
+  when :south
+    steps.times do
+      position[0] -= 1
+      record_position(visits, position)
+    end
+  when :west
+    steps.times do
+      position[1] -= 1
+      record_position(visits, position)
+    end
+  end
 end
 
-def required_ribbon(line)
-  a, b, c = line.split("x").map(&:to_i).sort
-  (2 * (a + b)) + (a * b * c)
+def record_position(visits, position)
+  key = position.to_s
+  abort(position.sum(&:abs).to_s) if visits.key?(key)
+  visits[key] = true
 end
 
-lines = File.readlines("input.txt")
-
-p(lines.sum { |l| required_wrapping_paper(l) })
-
-p(lines.sum { |l| required_ribbon(l) })
+bearing = %i[north east south west]
+position = [0, 0]
+visits = {}
+key = position.to_s
+visits[key] = true
+instructions.split(", ").each do |instruction|
+  turn, *steps = instruction.chars
+  steps = steps.join.to_i
+  case turn
+  when "L" then bearing.rotate!(-1)
+  when "R" then bearing.rotate!(+1)
+  end
+  take_steps(visits, position, bearing, steps)
+end

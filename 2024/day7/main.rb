834aea1b292ffc4d77ad713d714865fc7e46a7bd
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-07 06:44:08 -0500

    refactor

--- 2024/day7/main.rb
+++ 2024/day7/main.rb
@@ -4,12 +4,16 @@ def match?(equation)
   left_total, numbers = equation.split(":")
   left_total = left_total.to_i
   numbers = numbers.split.map(&:to_i)
-  operator_permutations = %i[+ *].repeated_permutation(numbers.count - 1)
+  operator_permutations = %i[+ * ||].repeated_permutation(numbers.count - 1)
   operator_permutations.any? do |operator_permutation|
     first, *rest = numbers
     right_total = first
     operator_permutation.zip(rest).each do |operator, number|
-      right_total = right_total.send(operator, number)
+      right_total = if operator == :"||"
+                      (right_total.to_s + number.to_s).to_i
+                    else
+                      right_total.send(operator, number)
+                    end
     end
     right_total == left_total
   end

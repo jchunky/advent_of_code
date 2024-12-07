1e827a9c01b887026a3468612f2c192b5282f463
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-07 06:39:23 -0500

    refactor

---
+++ 2024/day7/main.rb
@@ -0,0 +1,28 @@
+require "active_support/all"
+
+def match?(equation)
+  left_total, numbers = equation.split(":")
+  left_total = left_total.to_i
+  numbers = numbers.split.map(&:to_i)
+  operator_permutations = %i[+ *].repeated_permutation(numbers.count - 1)
+  operator_permutations.any? do |operator_permutation|
+    first, *rest = numbers
+    right_total = first
+    operator_permutation.zip(rest).each do |operator, number|
+      right_total = right_total.send(operator, number)
+    end
+    right_total == left_total
+  end
+end
+
+def total_of(equation)
+  equation.to_i
+end
+
+equations = File.readlines("input.txt").map(&:chomp)
+result =
+  equations
+    .select { |e| match?(e) }
+    .sum { |e| total_of(e) }
+
+p result

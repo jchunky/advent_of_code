026e24cd3ed1422a6d6a0873a5c29f2754d33332
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-08 10:14:06 -0500

    refactor

--- 2017/day3/main.rb
+++ 2017/day3/main.rb
@@ -1,32 +1,69 @@
 require "active_support/all"
 
+class Position < Struct.new(:row, :col)
+  def move(direction)
+    case direction
+    when :north then Position.new(row - 1, col)
+    when :east then Position.new(row, col + 1)
+    when :south then Position.new(row + 1, col)
+    when :west then Position.new(row, col - 1)
+    end
+  end
+end
+
+class Grid
+  attr_reader :positions
+
+  delegate :include?, to: :positions
+
+  def initialize
+    @positions = {}
+  end
+
+  def content_of(position)
+    positions[position] || 0
+  end
+
+  def place_content_at(position, content)
+    positions[position] = content
+  end
+end
+
+class Vector < Struct.new(:position, :directions)
+  def initialize(position = Position.new(0, 0), directions = %i[south east north west])
+    super
+  end
+
+  def left_position
+    dup.turn_left.step
+  end
+
+  def turn_left
+    directions.rotate!
+    self
+  end
+
+  def step
+    self.position = position.move(direction)
+  end
+
+  def dup
+    Vector.new(position.dup, directions.dup)
+  end
+
+  def direction
+    directions.first
+  end
+end
+
 square_index = 312051
 # square_index = 1024
 
-direction = %i[east north west south]
-index = 1
-step_index = 0
-step_count = 0
-step_target = 1
-x = 0
-y = 0
-
-while index < square_index
-  case direction.first
-  when :north then y += 1
-  when :east then x += 1
-  when :south then y -= 1
-  when :west then x -= 1
-  end
-
-  step_count += 1
-  index += 1
-  next unless step_count == step_target
-
-  step_index += 1
-  step_count = 0
-  step_target += 1 if step_index.even?
-  direction.rotate!
+vector = Vector.new
+grid = Grid.new
+(1..square_index - 1).each do |index|
+  grid.place_content_at(vector.position, index)
+  vector.turn_left unless grid.include?(vector.left_position)
+  vector.step
 end
-
-p [x, y].sum(&:abs)
+p [vector.position.row, vector.position.col].sum(&:abs) # 31, 430

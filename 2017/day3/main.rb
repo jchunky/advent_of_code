3dc5afa630182fea748d87ff759c98afaaf425f4
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-08 08:32:57 -0500

    refactor

--- 2017/day3/main.rb
+++ 2017/day3/main.rb
@@ -1,17 +1,32 @@
 require "active_support/all"
 
-def required_wrapping_paper(line)
-  a, b, c = line.split("x").map(&:to_i).sort
-  (a * b) + (2 * ((a * b) + (b * c) + (c * a)))
+square_index = 312051
+# square_index = 1024
+
+direction = %i[east north west south]
+index = 1
+step_index = 0
+step_count = 0
+step_target = 1
+x = 0
+y = 0
+
+while index < square_index
+  case direction.first
+  when :north then y += 1
+  when :east then x += 1
+  when :south then y -= 1
+  when :west then x -= 1
+  end
+
+  step_count += 1
+  index += 1
+  next unless step_count == step_target
+
+  step_index += 1
+  step_count = 0
+  step_target += 1 if step_index.even?
+  direction.rotate!
 end
 
-def required_ribbon(line)
-  a, b, c = line.split("x").map(&:to_i).sort
-  (2 * (a + b)) + (a * b * c)
-end
-
-lines = File.readlines("input.txt")
-
-p(lines.sum { |l| required_wrapping_paper(l) })
-
-p(lines.sum { |l| required_ribbon(l) })
+p [x, y].sum(&:abs)

3e423c6cce6ee3f7e1aed43fbb6506bd21dee1f7
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-08 14:16:21 -0500

    refactor

--- 2018/day1/main.rb
+++ 2018/day1/main.rb
@@ -3,3 +3,11 @@ require "active_support/all"
 lines = File.read("input.txt")
 
 p eval(lines.delete("\n")) # 587
+
+frequency = 0
+seen = {}
+lines.split("\n").map(&:to_i).cycle.each do |delta|
+  frequency += delta
+  abort(frequency.to_s) if seen.include?(frequency)
+  seen[frequency] = true
+end

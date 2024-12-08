e704a1094439152495bdaccee7a1502c96c75797
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-08 14:28:36 -0500

    refactor

--- 2018/day2/main.rb
+++ 2018/day2/main.rb
@@ -5,3 +5,12 @@ lines = File.read("input.txt").split("\n")
 pairs = lines.count { |line| line.chars.tally.value?(2) }
 triples = lines.count { |line| line.chars.tally.value?(3) }
 p pairs * triples # 8610
+
+candidates =
+  lines.combination(2).select do |a, b|
+    a.chars.zip(b.chars).count { |a, b|
+      a != b
+    } == 1
+  end
+
+p candidates

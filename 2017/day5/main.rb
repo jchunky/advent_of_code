65b63ea2fde5ed95222a37bcfe309ca25e35a501
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-08 11:40:36 -0500

    refactor

--- 2017/day5/main.rb
+++ 2017/day5/main.rb
@@ -13,10 +13,19 @@ instructions = lines.map(&:to_i)
 position = 0
 steps = 0
 
+# loop do
+#   jump = instructions[position]
+#   instructions[position] += 1
+#   position += jump
+#   steps += 1
+#   abort(steps.to_s) unless (0...instructions.count).cover?(position) # 5, 355965
+# end
+
 loop do
   jump = instructions[position]
-  instructions[position] += 1
+  increment = jump >= 3 ? -1 : 1
+  instructions[position] += increment
   position += jump
   steps += 1
-  abort(steps.to_s) unless (0...instructions.count).cover?(position) # 5, 355965
+  abort(steps.to_s) unless (0...instructions.count).cover?(position) # 26948068
 end

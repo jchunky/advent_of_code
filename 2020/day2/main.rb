84f0cab2bddaf5019307b888f62457e2512f4de8
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-08 22:54:23 -0500

    refactor

--- 2020/day2/main.rb
+++ 2020/day2/main.rb
@@ -1,13 +1,10 @@
 require "active_support/all"
 
-lines = File.read("input.txt")
+lines = File.read("input.txt").split("\n")
 
-p eval(lines.delete("\n")) # 587
-
-frequency = 0
-seen = {}
-lines.split("\n").map(&:to_i).cycle.each do |delta|
-  frequency += delta
-  abort(frequency.to_s) if seen.include?(frequency)
-  seen[frequency] = true
-end
+p(
+  lines.count do |line|
+    lower, upper, letter, _, password = line.split(/\W/)
+    (lower.to_i..upper.to_i).cover?(password.chars.tally[letter])
+  end
+)

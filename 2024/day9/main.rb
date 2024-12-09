822a51b0813ee4b7e2d2f35aae7a77efea178f5c
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-09 13:27:42 -0500

    refactor

--- 2024/day9/main.rb
+++ 2024/day9/main.rb
@@ -1,7 +1,7 @@
 require "active_support/all"
 
 input = File.read("input.txt")
-input = "2333133121414131402"
+# input = "2333133121414131402"
 
 # 00...111...2...333.44.5555.6666.777.888899
 
@@ -14,4 +14,16 @@ array =
     end
   end
 
-p array
+i = 0
+j = array.size - 1
+
+loop do
+  i += 1 while array[i] != "." && i < j
+  j -= 1 while array[j] == "." && i < j
+  break unless i < j
+
+  array[i] = array[j]
+  array[j] = "."
+end
+
+p(array.map.with_index { |value, index| value.to_i * index }.sum) # 1928, 6307275788409

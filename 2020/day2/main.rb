144c6a2f4e66f46aa37a2aecb1d3d3a9a37266cf
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-08 23:04:37 -0500

    refactor

--- 2020/day2/main.rb
+++ 2020/day2/main.rb
@@ -2,9 +2,23 @@ require "active_support/all"
 
 lines = File.read("input.txt").split("\n")
 
+# lines = "1-3 a: abcde
+# 1-3 b: cdefg
+# 2-9 c: ccccccccc
+# ".split("\n")
+
 p(
   lines.count do |line|
     lower, upper, letter, _, password = line.split(/\W/)
     (lower.to_i..upper.to_i).cover?(password.chars.tally[letter])
+  end
+)
+
+p(
+  lines.count do |line|
+    lower, upper, letter, _, password = line.split(/\W/)
+    a = password[lower.to_i - 1]
+    b = password[upper.to_i - 1]
+    a != b && [a, b].include?(letter)
   end
 )

39893a8ee9c726698cdc0aff7048a2f6750a4f60
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-08 22:45:07 -0500

    refactor

--- 2019/day1/main.rb
+++ 2019/day1/main.rb
@@ -2,4 +2,13 @@ require "active_support/all"
 
 lines = File.read("input.txt").split("\n")
 
+def required_fuel_for(mass)
+  fuel_mass = (mass / 3) - 2
+  return 0 if fuel_mass <= 0
+
+  fuel_mass + required_fuel_for(fuel_mass)
+end
+
 p(lines.map(&:to_i).sum { |mass| (mass / 3) - 2 })
+
+p(lines.map(&:to_i).sum { |mass| required_fuel_for(mass) })
--- 2019/day4/main.rb
+++ 2019/day4/main.rb
@@ -1,13 +1,24 @@
 require "active_support/all"
 
-lines = File.read("input.txt")
+input = (231832..767346)
 
-p eval(lines.delete("\n")) # 587
+p(
+  input.count do |n|
+    digits = n.to_s.chars
+    digits.each_cons(2).any? { |a, b| a == b } &&
+    digits == digits.sort
+  end
+)
 
-frequency = 0
-seen = {}
-lines.split("\n").map(&:to_i).cycle.each do |delta|
-  frequency += delta
-  abort(frequency.to_s) if seen.include?(frequency)
-  seen[frequency] = true
-end
+p(
+  input.count do |n|
+    digits = n.to_s.chars
+
+    (
+      (digits[0] == digits[1] && digits[1] != digits[2]) ||
+      (digits[-3] != digits[-2] && digits[-2] == digits[-1]) ||
+      digits.count.times.any? { |i| digits[i] != digits[i + 1] && digits[i + 1] == digits[i + 2] && digits[i + 2] != digits[i + 3] }
+    ) &&
+    digits == digits.sort
+  end
+)

75240c9c2c7a69108802eba2b79a0edb9c8aec6a
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-10 16:52:50 -0500

    refactor

--- lib/2024/day3/problem1.rb
+++ lib/2024/day3/problem1.rb
@@ -1,8 +1,11 @@
 module Year2024
   module Day3
     class Problem1 < Problem
+      def self.test_input
+        "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+      end
       def self.test_result
-        184511516
+        161
       end
 
       def result

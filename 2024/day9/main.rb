565905921c7275c048dde5b3700918cb38fdbe9a
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-09 21:22:13 -0500

    refactor

--- 2024/day9/main.rb
+++ 2024/day9/main.rb
@@ -2,7 +2,6 @@ require "active_support/all"
 
 input = File.read("input.txt").chomp
 # input = "2333133121414131402"
-p input
 
 # 00...111...2...333.44.5555.6666.777.888899
 
@@ -22,25 +21,26 @@ loop do
 end
 p(array.map.with_index { |value, index| value.to_i * index }.sum) # 1928, 6307275788409
 
+p array.size
 array =
   input.chars.flat_map.with_index do |char, i|
     (i.even? ? [i / 2] : ["."]) * char.to_i
   end
 tally = array.tally
-p(
-  (array.uniq - ["."]).reverse.each do |file_id|
-    file_position = array.index(file_id)
-    file_length = tally[file_id]
-    freespace_position = array.join.index("." * file_length)
-    # p [file_id, file_position, file_length, freespace_position]
-    next unless freespace_position && freespace_position < file_position
-
-    (freespace_position...freespace_position + file_length).each do |i|
-      array[i] = file_id
-    end
-    (file_position...file_position + file_length).each do |i|
-      array[i] = "."
-    end
+(array.uniq - ["."]).reverse_each.with_index do |file_id, i|
+  print "." if i % 1000 == 0
+  file_position = array.index(file_id)
+  file_length = tally[file_id]
+  freespace_position = array.length.times.find do |j|
+    (j...j + file_length).all? { |k| array[k] == "." }
   end
-)
+  next unless freespace_position && freespace_position < file_position
+
+  (freespace_position...freespace_position + file_length).each do |i|
+    array[i] = file_id
+  end
+  (file_position...file_position + file_length).each do |i|
+    array[i] = "."
+  end
+end
 p(array.map.with_index { |value, index| value.to_i * index }.sum) # 2858,

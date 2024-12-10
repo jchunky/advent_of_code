9b25dcffc7083431ff0985503263f4079bb9cd3f
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-09 21:42:33 -0500

    refactor

--- 2022/day1/main.rb
+++ 2022/day1/main.rb
@@ -3,3 +3,5 @@ require "active_support/all"
 input = File.read("input.txt")
 
 p input.split("\n\n").map { |elf| elf.split("\n").sum(&:to_i) }.max # 71471
+
+p input.split("\n\n").map { |elf| elf.split("\n").sum(&:to_i) }.max(3).sum # 211189

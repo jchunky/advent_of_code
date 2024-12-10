ec65658534accaad262f797fb9a75dd030463f8b
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-10 15:38:26 -0500

    refactor

--- spec/problems_spec.rb
+++ spec/problems_spec.rb
@@ -9,34 +9,6 @@ RSpec.configure do |config|
   config.profile_examples = true
 end
 
-describe "problems" do
-  it "are solved" do
-    problems = Dir["../lib/**/problem?.rb"]
-    problems.each do |problem|
-      p problem
-      clazz = class_of(problem)
-      input =
-        if clazz.respond_to?(:test_input)
-          clazz.test_input
-        else
-          input_file = input_file_of(problem)
-          File.read(input_file)
-        end
-      result = get_result { clazz.new(input).result }
-      expect(result).to eq(clazz.test_result)
-    end
-  end
-
-  it "current problem is solved" do
-    problem = Dir["../lib/2024/day10/problem1.rb"].first
-
-    p problem
-    clazz = class_of(problem)
-    input_file = input_file_of(problem)
-    input = File.read(input_file)
-    p(get_result { clazz.new(input).result })
-  end
-
   def get_result
     yield
   rescue ResultException => ex
@@ -52,4 +24,35 @@ describe "problems" do
     year, day, problem = problem_path.split(/\W/)[4, 3]
     input_file = "#{problem_path[0, problem_path.rindex("/")]}/input.txt"
   end
+
+
+describe "problems" do
+
+  problems = Dir["../lib/**/problem?.rb"]
+  problems.each do |problem|
+    clazz = class_of(problem)
+    it "#{clazz} is solved" do
+      input =
+      if clazz.respond_to?(:test_input)
+        clazz.test_input
+      else
+        input_file = input_file_of(problem)
+        File.read(input_file)
+      end
+      result = get_result { clazz.new(input).result }
+      expect(result).to eq(clazz.test_result)
+    end
+  end
+
+  it "current problem is solved" do
+    problem = Dir["../lib/2024/day10/problem1.rb"].first
+
+    p problem
+    clazz = class_of(problem)
+    input_file = input_file_of(problem)
+    input = File.read(input_file)
+    p(get_result { clazz.new(input).result })
+  end
+
 end
+

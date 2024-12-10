2e5f5b62e727b370d3c82dc16ba6b8fe1ca37a86
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-10 17:02:14 -0500

    refactor

--- lib/2024/day9/problem2.rb
+++ lib/2024/day9/problem2.rb
@@ -1,16 +1,16 @@
 module Year2024
   module Day9
     class Problem2 < Problem
+      def self.test_input
+        "2333133121414131402"
+        # 00...111...2...333.44.5555.6666.777.888899
+      end
+
       def self.test_result
-        6327174563252
+        2858
       end
 
       def result
-        return 6327174563252 # todo
-        # input = "2333133121414131402"
-
-        # 00...111...2...333.44.5555.6666.777.888899
-
         array =
           input.chars.flat_map.with_index do |char, i|
             (i.even? ? [i / 2] : ["."]) * char.to_i

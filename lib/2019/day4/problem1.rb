a4eadc64ff80a9a9dfb6a6df0d9e0b5e1012b77b
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-10 16:33:00 -0500

    refactor

--- lib/2019/day4/problem1.rb
+++ lib/2019/day4/problem1.rb
@@ -1,14 +1,19 @@
 module Year2019
   module Day4
     class Problem1 < Problem
+      def self.test_input
+        "100000,112000"
+      end
+
       def self.test_result
-        1330
+        165
       end
 
       def result
-        input = (231832..767346)
+        lower, upper = input.split(",").map(&:to_i)
+        range = (lower..upper)
 
-        input.count do |n|
+        range.count do |n|
           digits = n.to_s.chars
           digits.each_cons(2).any? { |a, b| a == b } &&
             digits == digits.sort
--- lib/2019/day4/problem2.rb
+++ lib/2019/day4/problem2.rb
@@ -1,14 +1,19 @@
 module Year2019
   module Day4
     class Problem2 < Problem
+      def self.test_input
+        "100000,112000"
+      end
+
       def self.test_result
-        876
+        64
       end
 
       def result
-        input = (231832..767346)
+        lower, upper = input.split(",").map(&:to_i)
+        range = (lower..upper)
 
-        input.count do |n|
+        range.count do |n|
           digits = n.to_s.chars
 
           digits.dup.prepend(nil).append(nil).each_cons(4).any? do |a, b, c, d|

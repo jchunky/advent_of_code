0a2c371dcb91a3e212b3be2823f664cd7699c088
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-10 16:38:24 -0500

    refactor

--- lib/2020/day2/problem1.rb
+++ lib/2020/day2/problem1.rb
@@ -1,8 +1,15 @@
 module Year2020
   module Day2
     class Problem1 < Problem
+      def self.test_input
+        "1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc
+"
+      end
+
       def self.test_result
-        383
+        2
       end
 
       def result
--- lib/2020/day2/problem2.rb
+++ lib/2020/day2/problem2.rb
@@ -1,16 +1,18 @@
 module Year2020
   module Day2
     class Problem2 < Problem
+      def self.test_input
+        "1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc
+"
+      end
+
       def self.test_result
-        272
+        1
       end
 
       def result
-        # lines = "1-3 a: abcde
-        # 1-3 b: cdefg
-        # 2-9 c: ccccccccc
-        # ".split("\n")
-
         lines.count do |line|
           lower, upper, letter, _, password = line.split(/\W/)
           a = password[lower.to_i - 1]

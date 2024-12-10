e26b3c9e4ee3e2338f649691c4d8fa73f164fc54
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-10 16:34:16 -0500

    refactor

--- lib/2020/day1/problem1.rb
+++ lib/2020/day1/problem1.rb
@@ -1,8 +1,17 @@
 module Year2020
   module Day1
     class Problem1 < Problem
+      def self.test_input
+        "1721
+979
+366
+299
+675
+1456"
+      end
+
       def self.test_result
-        988771
+        514579
       end
 
       def result

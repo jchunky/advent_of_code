11c9d3802e7cf3ec0b88e004ea46de6d5fa09d2e
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-10 16:15:02 -0500

    refactor

--- lib/2017/day4/problem1.rb
+++ lib/2017/day4/problem1.rb
@@ -1,8 +1,22 @@
 module Year2017
   module Day4
     class Problem1 < Problem
+      def self.test_input
+        "vxjtwn vjnxtw sxibvv mmws wjvtxn icawnd rprh
+fhaa qwy vqbq gsswej lxr yzl wakcige mwjrl
+bhnlow huqa gtbjc gvj wrkyr jgvmhj bgs umo ikbpdto
+drczdf bglmf gsx flcf ojpj kzrwrho owbkl dgrnv bggjevc
+ndncqdl lncaugj mfa lncaugj skt pkssyen rsb npjzf
+kdd itdyhe pvljizn cgi
+jgy pyhuq eecb phwkyl oeftyu pyhuq hecxgti tpadffm jgy
+zvc qdk mlmyj kybbh lgbb fvfzcer frmaxa yzgw podt dbycoii afj
+zfr msn mns leqem frz
+golnm ltizhd dvwv xrizqhd omegnez nan yqajse lgef
+"
+      end
+
       def self.test_result
-        451
+        8
       end
 
       def result

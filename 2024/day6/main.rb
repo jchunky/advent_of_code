dbb6bcc35effae2dbd7e5f768501ff22c1e53c50
Jason CheongKeeYou <jasonc@nulogy.com>
2024-12-10 05:41:55 -0500

    refactor

--- 2024/day6/main.rb
+++ 2024/day6/main.rb
@@ -15,56 +15,6 @@ input = "....#.....
 ......#...
 ".split("\n")
 
-class Guard < Struct.new(:grid, :position, :directions, :loop_found, :visits)
-  def initialize(grid, position)
-    super(grid, position, %i[north east south west], false, {})
-  end
-
-  def walk
-    while on_grid? && !in_loop?
-      record_visit
-      turn_right while facing_obstacle?
-      step while on_grid? && !facing_obstacle?
-    end
-  end
-
-  def in_loop?
-    loop_found
-  end
-
-  private
-
-  def record_visit
-    visit = [position.row, position.col, bearing].to_s
-    self.loop_found = true if visits.include?(visit)
-    visits[visit] = true
-  end
-
-  def on_grid?
-    grid.include?(position)
-  end
-
-  def turn_right
-    directions.rotate!
-  end
-
-  def facing_obstacle?
-    grid.obstacle_at?(next_position)
-  end
-
-  def step
-    self.position = next_position
-  end
-
-  def next_position
-    position.next_position(bearing)
-  end
-
-  def bearing
-    directions.first
-  end
-end
-
 class MyGrid < Grid
   def find_guard_position
     find_content("^")
@@ -112,7 +62,7 @@ class Map
   def variations_with_loop_count
     grid.each_variation.with_index.count do |grid, i|
       print "." if i % grid.rows.size == 0
-      guard = Guard.new(grid, starting_guard_position)
+      guard = Walker.new(grid, starting_guard_position)
       guard.walk
       guard.in_loop?
     end

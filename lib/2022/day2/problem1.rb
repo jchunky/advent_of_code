0407d42964445047fe1c73702c9c08d7430823aa
Jason CheongKeeYou <jasonc@nulogy.com>
2025-12-01 18:46:46 -0500

    2022, day 2, problem 1

---
+++ lib/2022/day2/problem1.rb
@@ -0,0 +1,32 @@
+module Year2022
+  module Day2
+    class Problem1 < Problem
+      def self.test_input
+        "A Y
+B X
+C Z
+"
+      end
+
+      def self.test_result
+        15
+      end
+
+      SCORE = {
+        "A X" => 1 + 3,
+        "A Y" => 2 + 6,
+        "A Z" => 3 + 0,
+        "B X" => 1 + 0,
+        "B Y" => 2 + 3,
+        "B Z" => 3 + 6,
+        "C X" => 1 + 6,
+        "C Y" => 2 + 0,
+        "C Z" => 3 + 3,
+      }
+
+      def result
+        lines.sum { SCORE[it] }
+      end
+    end
+  end
+end

7cbc2e284c22e397e93fd2453fd7682e005f56f1
Jason CheongKeeYou <jasonc@nulogy.com>
2025-12-01 18:31:46 -0500

    2021, day 2, problem 1

---
+++ lib/2021/day2/problem2.rb
@@ -0,0 +1,40 @@
+module Year2021
+  module Day2
+    class Problem2 < Problem
+      def self.test_inputx
+        "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+"
+      end
+
+      def self.test_result
+        150
+      end
+
+      def result
+        v = 0
+        h = 0
+
+        lines.each do |line|
+          direction, magnitude = line.split
+          magnitude = magnitude.to_i
+
+          case direction
+          when 'forward'
+            h += magnitude
+          when 'down'
+            v += magnitude
+          when 'up'
+            v -= magnitude
+          end
+        end
+
+        v * h
+      end
+    end
+  end
+end

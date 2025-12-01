e6c10490329910c7b2e9d9058a24a5858efcde32
Jason CheongKeeYou <jasonc@nulogy.com>
2025-12-01 17:27:20 -0500

    2025, day 1, problem 2

--- lib/2025/day1/problem2.rb
+++ lib/2025/day1/problem2.rb
@@ -1,19 +1,19 @@
 module Year2025
   module Day1
     class Problem2 < Problem
-      def self.test_input
-        "L68
-L30
-R48
-L5
-R60
-L55
-L1
-L99
-R14
-L82
-"
-      end
+#       def self.test_input
+#         "L68
+# L30
+# R48
+# L5
+# R60
+# L55
+# L1
+# L99
+# R14
+# L82
+# "
+#       end
 
       def self.test_result
         6
@@ -24,9 +24,12 @@ L82
         dial = (0..99).to_a
         dial.rotate!(50)
         lines.each do |line|
-          rotation = line.tr('LR', '-+').to_i
-          dial.rotate!(rotation)
-          zero_position_count += 1 if dial[0] == 0
+          direction = line[0] == 'L' ? -1 : 1
+          rotation = line[1..].to_i
+          rotation.times do
+            dial.rotate!(direction)
+            zero_position_count += 1 if dial[0] == 0
+          end
         end
         zero_position_count
       end

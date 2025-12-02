f9a73bf4b537ce0d96e42f9dbe825d4f1504d42f
Jason CheongKeeYou <jasonc@nulogy.com>
2025-12-02 08:09:56 -0500

    2021, day 3, problem 2

--- lib/2021/day3/problem2.rb
+++ lib/2021/day3/problem2.rb
@@ -2,40 +2,67 @@ module Year2021
   module Day3
     class Problem2 < Problem
       def self.test_input
-        "forward 5
-down 5
-forward 8
-up 3
-down 8
-forward 2
+        "00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010
 "
       end
 
       def self.test_result
-        900
+        230
       end
 
       def result
-        v = 0
-        h = 0
-        aim = 0
+        oxygen_generator_rating * co2_scrubber_rating
+      end
 
-        lines.each do |line|
-          direction, x = line.split
-          x = x.to_i
+      def oxygen_generator_rating
+        the_lines = lines
 
-          case direction
-          when 'forward'
-            h += x
-            v += aim * x
-          when 'down'
-            aim += x
-          when 'up'
-            aim -= x
-          end
+        index = 0
+
+        loop do
+          line_count = the_lines.count
+
+          return the_lines.first.to_i(2) if line_count == 1
+
+          one_count = the_lines.count { it[index] == '1' }
+          zero_count = line_count - one_count
+
+          keep_character = one_count >= zero_count ? '1' : '0'
+          the_lines = the_lines.select { it[index] == keep_character }
+
+          index += 1
         end
+      end
 
-        v * h
+      def co2_scrubber_rating
+        the_lines = lines
+
+        index = 0
+
+        loop do
+          line_count = the_lines.count
+
+          return the_lines.first.to_i(2) if line_count == 1
+
+          one_count = the_lines.count { it[index] == '1' }
+          zero_count = line_count - one_count
+
+          keep_character = one_count < zero_count ? '1' : '0'
+          the_lines = the_lines.select { it[index] == keep_character }
+
+          index += 1
+        end
       end
     end
   end

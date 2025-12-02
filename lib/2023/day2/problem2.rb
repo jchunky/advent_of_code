c57669f4354c32e0b43577d50c682a29ded44b4d
Jason CheongKeeYou <jasonc@nulogy.com>
2025-12-01 21:07:34 -0500

    2023, day 2, problem 2

--- lib/2023/day2/problem2.rb
+++ lib/2023/day2/problem2.rb
@@ -1,57 +1,34 @@
 module Year2023
   module Day2
     class Problem2 < Problem
-      DIGITS = {
-        "one" => 1,
-        "two" => 2,
-        "three" => 3,
-        "four" => 4,
-        "five" => 5,
-        "six" => 6,
-        "seven" => 7,
-        "eight" => 8,
-        "nine" => 9,
-        "1" => 1,
-        "2" => 2,
-        "3" => 3,
-        "4" => 4,
-        "5" => 5,
-        "6" => 6,
-        "7" => 7,
-        "8" => 8,
-        "9" => 9,
-      }
-
-      def self.test_input
-        "two1nine
-eightwothree
-abcone2threexyz
-xtwone3four
-4nineeightseven2
-zoneight234
-7pqrstsixteen
+      def self.test_inputx
+        "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 "
       end
 
       def self.test_result
-        281
+        2286
       end
 
       def result
         lines.sum do |line|
-          _index, left_digit =
-            DIGITS
-              .transform_keys { |word| line.index(word) }
-              .select { |index, _value| index }
-              .min_by { |index, _value| index }
+          game, draws = line.split(":")
+          game = game.split.last.to_i
 
-          _index, right_digit =
-            DIGITS
-              .transform_keys { |word| line.rindex(word) }
-              .select { |index, _value| index }
-              .max_by { |index, _value| index }
+          counts = Hash.new(0)
+          draws.split(";").each do |draw|
+            draw.split(",").each do |color_sample|
+              count, color = color_sample.strip.split
+              count = count.to_i
+              counts[color] = [counts[color], count].max
+            end
+          end
 
-          [left_digit, right_digit].join.to_i
+          counts["red"] * counts["green"] * counts["blue"]
         end
       end
     end

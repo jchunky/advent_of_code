b5380d2345e089b30b2bd23a92c2b9edae3683f2
Jason CheongKeeYou <jasonc@nulogy.com>
2025-12-02 19:08:39 -0500

    2025, day 2, problem 1

---
+++ lib/2025/day2/problem1.rb
@@ -0,0 +1,38 @@
+module Year2025
+  module Day2
+    class Problem1 < Problem
+      def self.test_input
+        "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
+"
+      end
+
+      def self.test_result
+        1227775554
+      end
+
+      def result
+        sum = 0
+        lines.first.split(",").each do |range|
+          lower, upper = (range.include?("-") ? range.split("-") : [range, range])
+          lower = lower.to_i
+          upper = upper.to_i
+          (lower..upper).each do |n|
+            sum += n unless valid?(n)
+          end
+        end
+        sum
+      end
+
+      def valid?(n)
+        s = n.to_s
+        return true if s.length.odd?
+
+        half_length = s.length / 2
+        first_half = s[0, half_length]
+        second_half = s[(half_length)..]
+
+        first_half != second_half
+      end
+    end
+  end
+end
---
+++ lib/2025/day2/problem2.rb
@@ -0,0 +1,38 @@
+module Year2025
+  module Day2
+    class Problem2 < Problem
+      def self.test_input
+        "L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+"
+      end
+
+      def self.test_result
+        6
+      end
+
+      def result
+        zero_position_count = 0
+        dial = (0..99).to_a
+        dial.rotate!(50)
+        lines.each do |line|
+          direction = line[0] == 'L' ? -1 : 1
+          rotation = line[1..].to_i
+          rotation.times do
+            dial.rotate!(direction)
+            zero_position_count += 1 if dial[0] == 0
+          end
+        end
+        zero_position_count
+      end
+    end
+  end
+end

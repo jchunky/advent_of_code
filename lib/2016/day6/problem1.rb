fc6e54b3d9fc4409894fddc3a509038c11c38700
Jason CheongKeeYou <jasonc@nulogy.com>
2025-12-02 22:01:01 -0500

    2016, day 6, problem 1

---
+++ lib/2016/day6/problem1.rb
@@ -0,0 +1,34 @@
+module Year2016
+  module Day6
+    class Problem1 < Problem
+      def self.test_input
+        "eedadn
+drvtee
+eandsr
+raavrd
+atevrs
+tsrnev
+sdttsa
+rasrtv
+nssdts
+ntnada
+svetve
+tesnvt
+vntsnd
+vrdear
+dvrsen
+enarar"
+      end
+
+      def self.test_result
+        "easter"
+      end
+
+      def result
+        lines.map(&:chars).transpose.map do |column|
+          column.max_by { |c| column.count(c) }
+        end.join
+      end
+    end
+  end
+end

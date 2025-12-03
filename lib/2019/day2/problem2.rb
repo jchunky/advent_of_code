d34b2c84b2f51bd47f981156056e7123cda6a2ae
Jason CheongKeeYou <jasonc@nulogy.com>
2025-12-02 21:35:21 -0500

    2019, day 2, problem 2

--- lib/2019/day2/problem2.rb
+++ lib/2019/day2/problem2.rb
@@ -7,15 +7,29 @@ module Year2019
       end
 
       def self.test_result
-        3500
+        910
       end
 
       def result
         list = lines.first.split(",").map(&:to_i)
         index = 0
 
-        # list[1] = 12
-        # list[2] = 2
+        target = 3500
+        # target = 19690720
+
+        (0..99).to_a.each do |noun|
+          (0..99).to_a.each do |verb|
+
+            return 100 * noun + verb if compute(list.dup, noun, verb) == target
+          end
+        end
+      end
+
+      def compute(list, noun, verb)
+        index = 0
+
+        list[1] = noun
+        list[2] = verb
 
         loop do
           operation, a, b, destination = list[index, 4]
@@ -31,6 +45,8 @@ module Year2019
         end
 
         list[0]
+      rescue => ex
+        -1
       end
     end
   end

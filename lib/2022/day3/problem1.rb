f0667fee2bb58c3f5c3abfa2acba0fc776e1ed7e
Jason CheongKeeYou <jasonc@nulogy.com>
2025-12-02 21:49:52 -0500

    2022, day 3, problem 1

---
+++ lib/2022/day3/problem1.rb
@@ -0,0 +1,31 @@
+module Year2022
+  module Day3
+    class Problem1 < Problem
+      def self.test_input
+        "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+"
+      end
+
+      def self.test_result
+        157
+      end
+
+      VALUES = (('a'..'z').to_a + ('A'..'Z').to_a).zip((1..52).to_a).to_h
+
+      def result
+        lines.sum do |line|
+          line = line.chars
+          half = line.length / 2
+          a, b = line.first(half), line.last(half)
+          letter = a.intersection(b).first
+          VALUES[letter]
+        end
+      end
+    end
+  end
+end

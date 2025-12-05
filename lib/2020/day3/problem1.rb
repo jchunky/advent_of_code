0c077a3b3415b86553ea4a0d9c2f22f550a41bf8
Jason CheongKeeYou <jasonc@nulogy.com>
2025-12-05 17:46:08 -0500

    2020, day 3, problem 1

--- lib/2020/day3/problem1.rb
+++ lib/2020/day3/problem1.rb
@@ -17,11 +17,51 @@ module Year2020
       end
 
       def self.test_result
-        2
+        7
+      end
+
+      class Map < Struct.new(:grid)
+        def solve
+          tree_count = 0
+          position = [0, 0]
+          tree_count += 1 if tree?(position)
+          while !bottom?(position)
+            position = apply_delta(position, [1, 3])
+            tree_count += 1 if tree?(position)
+          end
+          tree_count
+        end
+
+        def tree?(coord)
+          grid[coord[0]][coord[1]] == '#'
+        end
+
+        def bottom?(coord)
+          coord[0] == height - 1
+        end
+
+        def width
+          grid[0].size
+        end
+
+        def height
+          grid.size
+        end
+
+        def apply_delta(coord, delta)
+          [
+            [coord, delta].map(&:first).sum,
+            [coord, delta].map(&:last).sum % width
+          ]
+        end
+
+        def method_name
+
+        end
       end
 
       def result
-        2
+        Map.new(lines).solve
       end
     end
   end

3b25ae143004c9d0331f41ed7f179a5b928e7df4
Jason CheongKeeYou <jasonc@nulogy.com>
2025-12-05 18:31:54 -0500

    2016, day 7, problem 1

---
+++ lib/2016/day7/problem2.rb
@@ -0,0 +1,31 @@
+module Year2016
+  module Day7
+    class Problem2 < Problem
+      def self.test_input
+        "abba[mnop]qrst
+abcd[bddb]xyyx
+aaaa[qwer]tyui
+ioxxoj[asdfgh]zxcvbn
+"
+      end
+
+      def self.test_result
+        2
+      end
+
+      def result
+        lines.count(&method(:supports_tls?))
+      end
+
+      def supports_tls?(ip_address)
+        sequences = ip_address.gsub(/\[\w+\]/, ' ').split
+        hypernet_sequences = ip_address.scan(/\[(\w+)\]/).flatten
+        sequences.any?(&method(:has_abba?)) && hypernet_sequences.none?(&method(:has_abba?))
+      end
+
+      def has_abba?(sequence)
+        sequence.chars.each_cons(4).any? { it == it.reverse && it.uniq.count > 1 }
+      end
+    end
+  end
+end

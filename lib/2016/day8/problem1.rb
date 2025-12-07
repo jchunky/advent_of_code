fe110b5140e459eaa4a8350f8022b6f1c3bec028
Jason CheongKeeYou <jasonc@nulogy.com>
2025-12-07 16:54:45 -0500

    2016, day 8, problem 1

---
+++ lib/2016/day8/problem1.rb
@@ -0,0 +1,45 @@
+module Year2016
+  module Day8
+    class Problem1 < Problem
+      def self.test_input
+        "rect 3x2
+rotate column x=1 by 1
+rotate row y=0 by 4
+rotate column x=1 by 1
+"
+      end
+
+      def self.test_result
+        6
+      end
+
+      def result
+        screen = 6.times.map { ['.'] * 50 }
+
+        lines.each do |line|
+          case line
+          when /rect/
+            w, h = line.scan(/\d+/).map(&:to_i)
+            h.times do |r|
+              w.times do |c|
+                screen[r][c] = '#'
+              end
+            end
+          when /rotate column/
+            i, rotation = line.scan(/\d+/).map(&:to_i)
+            screen = screen.transpose
+            screen[i].rotate!(-rotation)
+            screen = screen.transpose
+          when /rotate row/
+            i, rotation = line.scan(/\d+/).map(&:to_i)
+            screen[i].rotate!(-rotation)
+          end
+
+          # puts ["", ""] + screen.map(&:join)
+        end
+
+        screen.flatten.count('#')
+      end
+    end
+  end
+end

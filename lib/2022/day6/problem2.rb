e8abe05757c0281bd06a63e64319a936cc1d237f
Jason CheongKeeYou <jasonc@nulogy.com>
2025-12-07 14:23:42 -0500

    2022, day 6, problem 1

---
+++ lib/2022/day6/problem2.rb
@@ -0,0 +1,22 @@
+module Year2022
+  module Day6
+    class Problem2 < Problem
+      def self.test_input
+        "mjqjpqmgbljsphdztnvjfqwrcgsmlb
+"
+      end
+
+      def self.test_result
+        7
+      end
+
+      def result
+        i = 0
+        loop do
+          return i + 4 if input[i, 4].chars.uniq.count == 4
+          i += 1
+        end
+      end
+    end
+  end
+end

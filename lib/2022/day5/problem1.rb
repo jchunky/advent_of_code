3fb131e3b704c03fbedc38c4635edbcc3ea4a110
Jason CheongKeeYou <jasonc@nulogy.com>
2025-12-07 10:14:59 -0500

    2022, day 5, problem 1

--- lib/2022/day5/problem1.rb
+++ lib/2022/day5/problem1.rb
@@ -20,32 +20,47 @@ move 1 from 1 to 2
 
       class Solver < Struct.new(:lines)
         def solve
-          return "CMZ"
-          stacks = []
-          (0...column_count).each do |c|
-            (0...row_count).each do |r|
-              stacks[r] ||= []
-              stacks[r][c] = stack_descriptions[row_count - r]
-            end
+          stacks = starting_stacks
+          instructions.each do |instruction|
+            count, from, to = instruction.scan(/\d+/)
+            count, from, to = [count, from, to].map(&:to_i)
+            from -= 1
+            to -= 1
+
+            count.times { stacks[to].push(stacks[from].pop) }
           end
+
+          stacks.map(&:last).join
         end
 
         private
 
+        def starting_stacks
+          result = []
+          (0...column_count).each do |c|
+            (0...row_count).each do |r|
+              value = stack_descriptions.fetch(r, [])[1 + 4 * c]
+              result[c] ||= []
+              result[c][row_count - r] = value
+            end
+          end
+          result.map { it.join.strip.chars }
+        end
+
         def stack_descriptions
-          lines.select { it.include?("[") }
+          @stack_descriptions ||= lines.select { it.include?("[") }
         end
 
         def instructions
-          lines.select { it.include?("move") }
+          @instructions ||= lines.select { it.include?("move") }
         end
 
         def row_count
-          lines.select { it.include?("[") }.size
+          @row_count ||= lines.select { it.include?("[") }.size
         end
 
         def column_count
-          lines.select { it.start?(" 1").split.last.to_i }
+          @column_count ||= lines.find { it.start_with?(" 1") }.split.last.to_i
         end
       end
 
--- lib/2022/day5/problem2.rb
+++ lib/2022/day5/problem2.rb
@@ -18,8 +18,54 @@ move 1 from 1 to 2
         "CMZ"
       end
 
+      class Solver < Struct.new(:lines)
+        def solve
+          stacks = starting_stacks
+          instructions.each do |instruction|
+            count, from, to = instruction.scan(/\d+/)
+            count, from, to = [count, from, to].map(&:to_i)
+            from -= 1
+            to -= 1
+
+            count.times { stacks[to].push(stacks[from].pop) }
+          end
+
+          stacks.map(&:last).join
+        end
+
+        private
+
+        def starting_stacks
+          result = []
+          (0...column_count).each do |c|
+            (0...row_count).each do |r|
+              value = stack_descriptions.fetch(r, [])[1 + 4 * c]
+              result[c] ||= []
+              result[c][row_count - r] = value
+            end
+          end
+          result.map { it.join.strip.chars }
+        end
+
+        def stack_descriptions
+          @stack_descriptions ||= lines.select { it.include?("[") }
+        end
+
+        def instructions
+          @instructions ||= lines.select { it.include?("move") }
+        end
+
+        def row_count
+          @row_count ||= lines.select { it.include?("[") }.size
+        end
+
+        def column_count
+          @column_count ||= lines.find { it.start_with?(" 1") }.split.last.to_i
+        end
+      end
+
       def result
-        "CMZ"
+        Solver.new(lines).solve
       end
     end
   end

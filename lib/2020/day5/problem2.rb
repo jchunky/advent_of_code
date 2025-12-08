e8422cca3db44f190886a096a8fcd31f805b24dd
Jason CheongKeeYou <jasonc@nulogy.com>
2025-12-07 21:26:11 -0500

    2020, day 5, problem 1

---
+++ lib/2020/day5/problem2.rb
@@ -0,0 +1,42 @@
+class Array
+  def first_half
+    first(size / 2)
+  end
+
+  def last_half
+    last(size / 2)
+  end
+end
+
+module Year2020
+  module Day5
+    class Problem2 < Problem
+      def self.test_input
+        "FBFBBFFRLR
+"
+      end
+
+      def self.test_result
+        357
+      end
+
+      def result
+        lines.map do |line|
+          chars = line.chars
+
+          row = (0..127).to_a
+          chars.first(7).each do |r|
+            row = ((r == 'F') ? row.first_half : row.last_half)
+          end
+
+          col = (0..7).to_a
+          chars.last(3).each do |c|
+            col = ((c == 'L') ? col.first_half : col.last_half)
+          end
+
+          row.first * 8 + col.first
+        end.max
+      end
+    end
+  end
+end

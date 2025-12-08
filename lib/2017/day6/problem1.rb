11cf83ac47249ac5b932fa7499050a0590c0dc92
Jason CheongKeeYou <jasonc@nulogy.com>
2025-12-07 23:17:12 -0500

    2017, day 6, problem 1

---
+++ lib/2017/day6/problem1.rb
@@ -0,0 +1,34 @@
+module Year2017
+  module Day6
+    class Problem1 < Problem
+      def self.test_input
+        "0 2 7 0
+"
+      end
+
+      def self.test_result
+        5
+      end
+
+      def result
+        history = {}
+        banks = input.split.map(&:to_i)
+        cycles = 0
+        loop do
+          cycles += 1
+          i = banks.index(banks.max)
+          amount = banks[i]
+          banks[i] = 0
+          while amount > 0
+            i = (i + 1) % banks.size
+            banks[i] += 1
+            amount -= 1
+          end
+          snapshot = banks.to_s
+          return cycles if history[snapshot]
+          history[snapshot] = true
+        end
+      end
+    end
+  end
+end
